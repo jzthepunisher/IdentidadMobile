81250e01c19e72a0b511d54c2a0abf9e4011edce
jzthepunisher <setfocusjz@hotmail.com>
2017-04-14 00:26:59 -0500

    a

--- app/src/main/java/com/soloparaapasionados/identidadmobile/actividades/EmpleadoActivity.java
+++ app/src/main/java/com/soloparaapasionados/identidadmobile/actividades/EmpleadoActivity.java
@@ -2,6 +2,8 @@ package com.soloparaapasionados.identidadmobile.actividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.soloparaapasionados.identidadmobile.R;
 
@@ -11,5 +13,32 @@ public class EmpleadoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empleado);
+
+        setToolbar();
     }
+
+    private void setToolbar() {
+        // Añadir la Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Registro de Empleado");
+
+
+        if (getSupportActionBar() != null) // Habilitar up button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_empleado, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
 }
