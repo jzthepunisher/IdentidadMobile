478017b6ef5e60acaa12d954e1035ebd6ac98b27
jzthepunisher <setfocusjz@hotmail.com>
2017-05-09 00:04:18 -0500

    a

--- app/src/main/java/com/soloparaapasionados/identidadmobile/actividades/EmpleadoAdicionarEditarActivity.java
+++ app/src/main/java/com/soloparaapasionados/identidadmobile/actividades/EmpleadoAdicionarEditarActivity.java
@@ -1,32 +1,77 @@
 package com.soloparaapasionados.identidadmobile.actividades;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 import com.soloparaapasionados.identidadmobile.R;
+import com.soloparaapasionados.identidadmobile.fragmentos.EmpleadoAdicionarEditarFragment;
 
 public class EmpleadoAdicionarEditarActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ID_EMPLEADO = "extra_id_empleado";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empleado_adicionar_editar);
 
+        //Establec el toolbar de la actividad.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapser);
+
+        String idEmpleado = getIntent().getStringExtra(EmpleadoAdicionarEditarActivity.EXTRA_ID_EMPLEADO);
+
+        collapsingToolbarLayout.setTitle(idEmpleado == null ? "Añadir Empleado" : "Editar Empleado");
+
+
+        EmpleadoAdicionarEditarFragment empleadoAdicionarEditarFragment = (EmpleadoAdicionarEditarFragment)
+            getSupportFragmentManager().findFragmentById(R.id.empleado_adicionar_editar_container);
+
+        if (empleadoAdicionarEditarFragment == null) {
+            empleadoAdicionarEditarFragment = EmpleadoAdicionarEditarFragment.newInstance(idEmpleado);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.empleado_adicionar_editar_container, empleadoAdicionarEditarFragment)
+                    .commit();
+        }
+
+
+        FloatingActionButton floatingActionButtonGuardar = (FloatingActionButton) findViewById(R.id.floatingActionButtonGuardar);
+        floatingActionButtonGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Validar los datos
+
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_empleado_adicionar_editar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
 }
+
+
+
--- app/src/main/java/com/soloparaapasionados/identidadmobile/fragmentos/EmpleadoAdicionarEditarFragment.java
+++ app/src/main/java/com/soloparaapasionados/identidadmobile/fragmentos/EmpleadoAdicionarEditarFragment.java
@@ -2,12 +2,22 @@ package com.soloparaapasionados.identidadmobile.fragmentos;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.soloparaapasionados.identidadmobile.R;
+
+import java.util.regex.Pattern;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,34 +25,48 @@ import com.soloparaapasionados.identidadmobile.R;
  * create an instance of this fragment.
  */
 public class EmpleadoAdicionarEditarFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // Lista TextInputEditText
+    private TextInputLayout textInputLayoutIdEmpleado;
+    private TextInputLayout textInputLayoutNombresEmpleado;
+    private TextInputLayout textInputLayoutApellidoPaternoEmpleado;
+    private TextInputLayout textInputLayoutApellidoMaternoEmpleado;
+    private TextInputLayout textInputLayoutDireccionEmpleado;
+    private TextInputLayout textInputLayoutDniEmpleado;
+    private TextInputLayout textInputLayoutCelular;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutFechaNacimiento;
+    private TextInputLayout textInputLayoutFechaIngreso;
+    private TextInputLayout textInputLayoutFechaBaja;
+    // Lista EditText
+    private EditText editTextIdEmpleado;
+    private EditText editTextNombresEmpleado;
+    private EditText editTextApellidoPaternoEmpleado;
+    private EditText editTextApellidoMaternoEmpleado;
+    private EditText editTextDireccionEmpleado;
+    private EditText editTextDniEmpleado;
+    private EditText editTextCelular;
+    private EditText editTextEmail;
+    private EditText editTextFechaNacimiento;
+    private EditText editTextFechaIngreso;
+    private EditText editTextFechaBaja;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FloatingActionButton floatingActionButtonGuardar;
 
+
+
+    private static final String ARGUMENTO_ID_EMPLEADO = "argumento_id_empleado";
+    private String mIdEmpleado;
 
     public EmpleadoAdicionarEditarFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EmpleadoAdicionarEditarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EmpleadoAdicionarEditarFragment newInstance(String param1, String param2) {
+    public static EmpleadoAdicionarEditarFragment newInstance(String idEmpleado) {
         EmpleadoAdicionarEditarFragment fragment = new EmpleadoAdicionarEditarFragment();
+
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARGUMENTO_ID_EMPLEADO, idEmpleado);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,8 +75,7 @@ public class EmpleadoAdicionarEditarFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mIdEmpleado = getArguments().getString(ARGUMENTO_ID_EMPLEADO);
         }
     }
 
@@ -60,7 +83,283 @@ public class EmpleadoAdicionarEditarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empleado_adicionar_editar, container, false);
+        View root = inflater.inflate(R.layout.fragment_empleado_adicionar_editar, container, false);
+        //Referencias TextInputLayout de la UI
+        textInputLayoutIdEmpleado =(TextInputLayout) root.findViewById(R.id.textInputLayoutIdEmpleado);
+        textInputLayoutNombresEmpleado=(TextInputLayout) root.findViewById(R.id.textInputLayoutNombresEmpleado);
+        textInputLayoutApellidoPaternoEmpleado=(TextInputLayout) root.findViewById(R.id.textInputLayoutApellidoPaternoEmpleado);
+        textInputLayoutApellidoMaternoEmpleado=(TextInputLayout) root.findViewById(R.id.textInputLayoutApellidoMaternoEmpleado);
+        textInputLayoutDireccionEmpleado=(TextInputLayout) root.findViewById(R.id.textInputLayoutDireccionEmpleado);
+        textInputLayoutDniEmpleado=(TextInputLayout) root.findViewById(R.id.textInputLayoutDniEmpleado);
+        textInputLayoutCelular=(TextInputLayout) root.findViewById(R.id.textInputLayoutCelular);
+        textInputLayoutEmail=(TextInputLayout) root.findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutFechaNacimiento=(TextInputLayout) root.findViewById(R.id.textInputLayoutFechaNacimiento);
+        textInputLayoutFechaIngreso=(TextInputLayout) root.findViewById(R.id.textInputLayoutFechaIngreso);
+        textInputLayoutFechaBaja=(TextInputLayout) root.findViewById(R.id.textInputLayoutFechaBaja);
+        //Referencias EditText de la UI
+        editTextIdEmpleado=(EditText) root.findViewById(R.id.editTextIdEmpleado);
+        editTextNombresEmpleado=(EditText) root.findViewById(R.id.editTextNombresEmpleado);
+        editTextApellidoPaternoEmpleado=(EditText) root.findViewById(R.id.editTextApellidoPaternoEmpleado);
+        editTextApellidoMaternoEmpleado=(EditText) root.findViewById(R.id.editTextApellidoMaternoEmpleado);
+        editTextDireccionEmpleado=(EditText) root.findViewById(R.id.editTextDireccionEmpleado);
+        editTextDniEmpleado=(EditText) root.findViewById(R.id.editTextDniEmpleado);
+        editTextCelular=(EditText) root.findViewById(R.id.editTextCelular);
+        editTextEmail=(EditText) root.findViewById(R.id.editTextEmail);
+        editTextFechaNacimiento=(EditText) root.findViewById(R.id.editTextFechaNacimiento);
+        editTextFechaIngreso=(EditText) root.findViewById(R.id.editTextFechaIngreso);
+        editTextFechaBaja=(EditText) root.findViewById(R.id.editTextFechaBaja);
+
+        editTextIdEmpleado.addTextChangedListener(new MiTextWatcher(editTextIdEmpleado));
+        editTextNombresEmpleado.addTextChangedListener(new MiTextWatcher(editTextNombresEmpleado));
+        editTextApellidoPaternoEmpleado.addTextChangedListener(new MiTextWatcher(editTextApellidoPaternoEmpleado));
+        editTextApellidoMaternoEmpleado.addTextChangedListener(new MiTextWatcher(editTextApellidoMaternoEmpleado));
+
+        floatingActionButtonGuardar = (FloatingActionButton) getActivity().findViewById(R.id.floatingActionButtonGuardar);
+
+        // Eventos
+        floatingActionButtonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarDatosEmpleado();
+            }
+        });
+
+        return root;
+    }
+
+    private void validarDatosEmpleado() {
+        boolean error = false;
+
+        String IdEmpleado = editTextIdEmpleado.getText().toString();
+        String NombresEmpleado=editTextNombresEmpleado.getText().toString();
+        String ApellidoPaternoEmpleado=editTextApellidoPaternoEmpleado.getText().toString();
+        String ApellidoMaternoEmpleado=editTextApellidoMaternoEmpleado.getText().toString();
+        String DireccionEmpleado=editTextDireccionEmpleado.getText().toString();
+        String DniEmpleado=editTextDniEmpleado.getText().toString();
+        String Celular=editTextCelular.getText().toString();
+        String Email=editTextEmail.getText().toString();
+        String FechaNacimiento=editTextFechaNacimiento.getText().toString();
+        String FechaIngreso=editTextFechaIngreso.getText().toString();
+        String FechaBaja=editTextFechaBaja.getText().toString();
+
+
+        if (!esIdEmpleadoValido()) {
+            error = true;
+        }
+
+        if (!esNombreEmpleadoValido()) {
+            error = true;
+        }
+
+        if (!esApelllidoPaternoValido()) {
+            error = true;
+        }
+
+        /*if (TextUtils.isEmpty(ApellidoPaternoEmpleado)) {
+            textInputLayoutApellidoPaternoEmpleado.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutApellidoPaternoEmpleado.requestFocus();
+            error = true;
+            return;
+        }
+
+        if (TextUtils.isEmpty(ApellidoMaternoEmpleado)) {
+            textInputLayoutApellidoMaternoEmpleado.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutApellidoMaternoEmpleado.requestFocus();
+            error = true;
+            return;
+        }
+
+        if (TextUtils.isEmpty(DireccionEmpleado)) {
+            textInputLayoutDireccionEmpleado.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutDireccionEmpleado.requestFocus();
+            error = true;
+            return;
+        }
+
+        if (TextUtils.isEmpty(DniEmpleado)) {
+            textInputLayoutDniEmpleado.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutDniEmpleado.requestFocus();
+            error = true;
+            return;
+        }
+
+        if (TextUtils.isEmpty(Celular)) {
+            textInputLayoutCelular.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutCelular.requestFocus();
+            error = true;
+            return;
+        }
+
+        if (TextUtils.isEmpty(Email)) {
+            textInputLayoutEmail.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutEmail.requestFocus();
+            error = true;
+            return;
+        }
+
+        if (TextUtils.isEmpty(FechaNacimiento)) {
+            textInputLayoutFechaNacimiento.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutFechaNacimiento.requestFocus();
+            error = true;
+            return;
+        }
+
+        if (TextUtils.isEmpty(FechaIngreso)) {
+            textInputLayoutFechaIngreso.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutFechaIngreso.requestFocus();
+            error = true;
+            return;
+        }
+
+        if (TextUtils.isEmpty(FechaBaja)) {
+            textInputLayoutFechaBaja.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutFechaBaja.requestFocus();
+            error = true;
+            return;
+        }*/
+
+        if (error) {
+            return;
+        }
+
+        Toast.makeText(getActivity(), "Thank You!", Toast.LENGTH_SHORT).show();
+
+        //boolean a = esNombreValido(nombre);
+        //boolean b = esTelefonoValido(telefono);
+        //boolean c = esCorreoValido(correo);
+
+        //if (a && b && c) {
+        //    // OK, se pasa a la siguiente acción
+        //   Toast.makeText(this, "Se guarda el registro", Toast.LENGTH_LONG).show();
+        //}
+
+    }
+
+    private class MiTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MiTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            int identificadorVista=view.getId();
+            switch (identificadorVista)
+            {
+                case R.id.editTextIdEmpleado:
+                    esIdEmpleadoValido();
+                    break;
+                case R.id.editTextNombresEmpleado:
+                    esNombreEmpleadoValido();
+                    break;
+                case R.id.editTextApellidoPaternoEmpleado:
+                    esApelllidoPaternoValido();
+                    break;
+                case R.id.editTextApellidoMaternoEmpleado:
+                    esApelllidoMaternoValido();
+                    break;
+            }
+        }
+    }
+
+    //IdEmpleado
+    private boolean esIdEmpleadoValido() {
+        String idEmpleado=editTextIdEmpleado.getText().toString().trim();
+
+        if (idEmpleado.isEmpty()) {
+            textInputLayoutIdEmpleado.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutIdEmpleado.requestFocus();
+            return false;
+        }
+
+        if (idEmpleado.length() > 20) {
+            textInputLayoutIdEmpleado.setError(getString(R.string.error_campo_grande));
+            textInputLayoutIdEmpleado.requestFocus();
+            return false;
+        }
+
+        textInputLayoutIdEmpleado.setErrorEnabled(false);
+
+        return true;
+    }
+
+    //Nombres del Empleado
+    private boolean esNombreEmpleadoValido() {
+        String nombreEmpleado=editTextNombresEmpleado.getText().toString().trim();
+
+        if (nombreEmpleado.isEmpty()) {
+            textInputLayoutNombresEmpleado.setError(getString(R.string.error_campo_vacio));
+            textInputLayoutNombresEmpleado.requestFocus();
+            return false;
+        }
+
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        if (!patron.matcher(nombreEmpleado).matches()) {
+            textInputLayoutNombresEmpleado.setError(getString(R.string.error_campo_invalido_nombre_empleado));
+            textInputLayoutNombresEmpleado.requestFocus();
+            return false;
+        }
+
+        if (nombreEmpleado.length() > 50) {
+            textInputLayoutNombresEmpleado.setError(getString(R.string.error_campo_grande));
+            textInputLayoutNombresEmpleado.requestFocus();
+            return false;
+        }
+
+        textInputLayoutNombresEmpleado.setErrorEnabled(false);
+
+        return true;
+    }
+
+    //Apellido Paterno del Empleado
+    private boolean esApelllidoPaternoValido() {
+        String apelllidoPaterno=editTextApellidoPaternoEmpleado.getText().toString().trim();
+
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        if (!patron.matcher(apelllidoPaterno).matches()) {
+            textInputLayoutApellidoPaternoEmpleado.setError(getString(R.string.error_campo_invalido_apellido_paterno_empleado));
+            textInputLayoutApellidoPaternoEmpleado.requestFocus();
+            return false;
+        }
+
+        if (apelllidoPaterno.length() > 30) {
+            textInputLayoutApellidoPaternoEmpleado.setError(getString(R.string.error_campo_grande));
+            textInputLayoutApellidoPaternoEmpleado.requestFocus();
+            return false;
+        }
+
+        textInputLayoutApellidoPaternoEmpleado.setErrorEnabled(false);
+
+        return true;
+    }
+
+    //Apellido Paterno del Empleado
+    private boolean esApelllidoMaternoValido() {
+        String apelllidoMaterno=editTextApellidoMaternoEmpleado.getText().toString().trim();
+
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        if (!patron.matcher(apelllidoMaterno).matches() ) {
+            textInputLayoutApellidoMaternoEmpleado.setError(getString(R.string.error_campo_invalido_apellido_materno_empleado));
+            textInputLayoutApellidoMaternoEmpleado.requestFocus();
+            return false;
+        }
+
+        if (apelllidoMaterno.length() > 30) {
+            textInputLayoutApellidoMaternoEmpleado.setError(getString(R.string.error_campo_grande));
+            textInputLayoutApellidoMaternoEmpleado.requestFocus();
+            return false;
+        }
+
+        textInputLayoutApellidoMaternoEmpleado.setErrorEnabled(false);
+
+        return true;
     }
 
 }
